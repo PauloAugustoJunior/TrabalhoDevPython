a2266d8d3921a46e2fffda47181b6fdd7dfb9515
Coutinhopmw <cassiocoutinho258@gmail.com>
2023-11-06 10:57:30 -0300

    Criando a model da app

--- app/models.py
+++ app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Usuario(models.model):
+    nome = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    senha = models.CharField(max_length=255)
